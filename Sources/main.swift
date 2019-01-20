f299d446a0bf46725f423c7c029287c5aae29781
Serhii Londar <serhii.londar@gmail.com>
2019-01-20 12:18:11 +0200

    last changes

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,23 +1,25 @@
 import Foundation
-import MagickWand
+import CMagickWand
 
 
 var diameter = 640.0
 var radius = diameter / 2.0
 
-MagickWandGenesis();
+MagickWandGenesis()
 
-var m_wand = NewMagickWand();
-var d_wand = NewDrawingWand();
-var c_wand = NewPixelWand();
+var m_wand = NewMagickWand()
+var d_wand = NewDrawingWand()
+var c_wand = NewPixelWand()
 
-PixelSetColor(c_wand,"white");
-MagickNewImage(m_wand, Int(diameter), Int(diameter),c_wand);
+PixelSetColor(c_wand,"rgb(0,0,1)")
+MagickNewImage(m_wand, Int(diameter), Int(diameter), c_wand)
 
-DrawSetStrokeOpacity(d_wand,1);
+
+DrawSetStrokeOpacity(d_wand,1)
+let image = Image()
 
 // circle and rectangle
-PushDrawingWand(d_wand);
+PushDrawingWand(d_wand)
 // Hmmmm. Very weird. rgb(0,0,1) draws a black line around the edge
 // of the circle as it should. But rgb(0,0,0) or black don't.
 // AND if I remove the PixelSetColor then it draws a white boundary
@@ -27,54 +29,60 @@ PixelSetColor(c_wand,"rgb(0,0,1)")
 DrawSetStrokeColor(d_wand,c_wand)
 DrawSetStrokeWidth(d_wand,4)
 DrawSetStrokeAntialias(d_wand, MagickTrue)
-PixelSetColor(c_wand,"red");
-//DrawSetStrokeOpacity(d_wand,1);
-DrawSetFillColor(d_wand,c_wand);
+PixelSetColor(c_wand,"red")
+//DrawSetStrokeOpacity(d_wand,1)
+DrawSetFillColor(d_wand,c_wand)
 
-DrawCircle(d_wand,radius,radius,radius,radius*2);
-DrawRectangle(d_wand,50,13,120,87);
-PopDrawingWand(d_wand);
+// Set up the font size and colour
+DrawSetFont(d_wand,"Helvetica")
+PixelSetColor(c_wand,"black")
+DrawSetFillColor(d_wand,c_wand)
+DrawSetFontSize(d_wand,30)
+// Now draw the text
+DrawAnnotation(d_wand,18,30,"Put your text here")
+
+// same font - different colour and size
+PixelSetColor(c_wand,"yellow")
+DrawSetFillColor(d_wand,c_wand)
+DrawSetFontSize(d_wand,18)
+// Now draw the text
+DrawAnnotation(d_wand,30,90,"Put more text here")
+
+// same font, size and colour
+DrawAnnotation(d_wand,180,90,"Put the third text here")
+
+DrawCircle(d_wand,radius,radius,radius,radius*2)
+DrawRectangle(d_wand,50,13,120,87)
+PopDrawingWand(d_wand)
 
 // rounded rectangle
 PushDrawingWand(d_wand)
 
-var points: [PointInfo] =
-        [PointInfo(x: 378.1, y: 81.72)]
-//            { , }, { 381.1,79.56 }, { 384.3,78.12 }, { 387.6,77.33 },
-//            { 391.1,77.11 }, { 394.6,77.62 }, { 397.8,78.77 }, { 400.9,80.57 },
-//            { 403.6,83.02 }, { 523.9,216.8 }, { 526.2,219.7 }, { 527.6,223 },
-//            { 528.4,226.4 }, { 528.6,229.8 }, { 528,233.3 },   { 526.9,236.5 },
-//            { 525.1,239.5 }, { 522.6,242.2 }, { 495.9,266.3 }, { 493,268.5 },
-//            { 489.7,269.9 }, { 486.4,270.8 }, { 482.9,270.9 }, { 479.5,270.4 },
-//            { 476.2,269.3 }, { 473.2,267.5 }, { 470.4,265 },   { 350,131.2 },
-//            { 347.8,128.3 }, { 346.4,125.1 }, { 345.6,121.7 }, {345.4,118.2 },
-//            { 346,114.8 },   { 347.1,111.5 }, { 348.9,108.5 }, { 351.4,105.8 },
-//            { 378.1,81.72 }
-//    ]
-    
-    DrawSetStrokeAntialias(d_wand,MagickTrue);
-    DrawSetStrokeWidth(d_wand,2.016);
-    DrawSetStrokeLineCap(d_wand,RoundCap);
-    DrawSetStrokeLineJoin(d_wand,RoundJoin);
-    DrawSetStrokeDashArray(d_wand, 0, nil);
-    
-    PixelSetColor(c_wand,/*"#000080"*/"rgb(0,0,128)")
-    /* If strokecolor is removed completely then the circle is not there */
-    DrawSetStrokeColor(d_wand,c_wand);
-    /* But now I've added strokeopacity - 1=circle there  0=circle not there */
-    /* If opacity is 1 the black edge around the rectangle is visible */
-    DrawSetStrokeOpacity(d_wand,1);
-    /* No effect */
-    //        DrawSetFillRule(d_wand,EvenOddRule);
-    /* this doesn't affect the circle */
-    PixelSetColor(c_wand,"#c2c280"/*"rgb(194,194,128)"*/);
-    DrawSetFillColor(d_wand,c_wand);
-    //1=circle there 0=circle there but rectangle fill disappears
-    //		DrawSetFillOpacity(d_wand,0);
-    DrawPolygon(d_wand,37,points);
-    //		DrawSetStrokeOpacity(d_wand,1);
+var points: [PointInfo] = [PointInfo(x: 378.1, y: 81.72)]
 
-PopDrawingWand(d_wand);
+DrawSetStrokeAntialias(d_wand,MagickTrue)
+DrawSetStrokeWidth(d_wand,2.016)
+DrawSetStrokeLineCap(d_wand,RoundCap)
+DrawSetStrokeLineJoin(d_wand,RoundJoin)
+DrawSetStrokeDashArray(d_wand, 0, nil)
+
+PixelSetColor(c_wand,/*"#000080"*/"rgb(0,0,128)")
+/* If strokecolor is removed completely then the circle is not there */
+DrawSetStrokeColor(d_wand,c_wand)
+/* But now I've added strokeopacity - 1=circle there  0=circle not there */
+/* If opacity is 1 the black edge around the rectangle is visible */
+DrawSetStrokeOpacity(d_wand,1)
+/* No effect */
+//        DrawSetFillRule(d_wand,EvenOddRule)
+/* this doesn't affect the circle */
+PixelSetColor(c_wand,"#c2c280"/*"rgb(194,194,128)"*/)
+DrawSetFillColor(d_wand,c_wand)
+//1=circle there 0=circle there but rectangle fill disappears
+//		DrawSetFillOpacity(d_wand,0)
+DrawPolygon(d_wand,37,points)
+//		DrawSetStrokeOpacity(d_wand,1)
+
+PopDrawingWand(d_wand)
 
 
 /* yellow polygon */
@@ -85,21 +93,21 @@ points = [PointInfo(x: 540, y: 288)]
 //            { 302.4,194.4 }, { 331.2,64.8 },  { 504,64.8 },    { 475.2,115.2 },
 //            { 525.6,93.6 },  { 496.8,158.4 }, { 532.8,136.8 }, { 518.4,180 },
 //            { 540,172.8 },   { 540,223.2 },   { 540,288 }
-//    };
+//    }
 
-DrawSetStrokeAntialias(d_wand,MagickTrue);
-DrawSetStrokeWidth(d_wand,5.976);
-DrawSetStrokeLineCap(d_wand,RoundCap);
-DrawSetStrokeLineJoin(d_wand,RoundJoin);
-DrawSetStrokeDashArray(d_wand,0,nil);
-PixelSetColor(c_wand,"#4000c2");
-DrawSetStrokeColor(d_wand,c_wand);
-DrawSetFillRule(d_wand,EvenOddRule);
-PixelSetColor(c_wand,"#ffff00");
-DrawSetFillColor(d_wand,c_wand);
-DrawPolygon(d_wand,15,points);
+DrawSetStrokeAntialias(d_wand,MagickTrue)
+DrawSetStrokeWidth(d_wand,5.976)
+DrawSetStrokeLineCap(d_wand,RoundCap)
+DrawSetStrokeLineJoin(d_wand,RoundJoin)
+DrawSetStrokeDashArray(d_wand,0,nil)
+PixelSetColor(c_wand,"#4000c2")
+DrawSetStrokeColor(d_wand,c_wand)
+DrawSetFillRule(d_wand,EvenOddRule)
+PixelSetColor(c_wand,"#ffff00")
+DrawSetFillColor(d_wand,c_wand)
+DrawPolygon(d_wand,15,points)
 
-PopDrawingWand(d_wand);
+PopDrawingWand(d_wand)
 
 // rotated and translated ellipse
 // The DrawEllipse function only draws the ellipse with
@@ -116,34 +124,35 @@ PopDrawingWand(d_wand);
 // NOTE that doing all this within PushDrawingWand()/PopDrawingWand()
 // means that the coordinate system will be restored after
 // the PopDrawingWand
-PushDrawingWand(d_wand);
+PushDrawingWand(d_wand)
 
-PixelSetColor(c_wand,"rgb(0,0,1)");
+PixelSetColor(c_wand,"rgb(0,0,1)")
 
-DrawSetStrokeColor(d_wand,c_wand);
-DrawSetStrokeWidth(d_wand,2);
-DrawSetStrokeAntialias(d_wand,MagickTrue);
-PixelSetColor(c_wand,"orange");
-//DrawSetStrokeOpacity(d_wand,1);
-DrawSetFillColor(d_wand,c_wand);
+DrawSetStrokeColor(d_wand,c_wand)
+DrawSetStrokeWidth(d_wand,2)
+DrawSetStrokeAntialias(d_wand,MagickTrue)
+PixelSetColor(c_wand,"orange")
+//DrawSetStrokeOpacity(d_wand,1)
+DrawSetFillColor(d_wand,c_wand)
 // Be careful of the order in which you meddle with the
 // coordinate system! Rotating and then translating is
 // not the same as translating then rotating
-DrawTranslate(d_wand,radius/2,3*radius/2);
-DrawRotate(d_wand,-30);
-DrawEllipse(d_wand,0,0,radius/8,3*radius/8,0,360);
+DrawTranslate(d_wand,radius/2,3*radius/2)
+DrawRotate(d_wand,-30)
+DrawEllipse(d_wand,0,0,radius/8,3*radius/8,0,360)
 
-PopDrawingWand(d_wand);
+PopDrawingWand(d_wand)
 
 // A line from the centre of the circle
 // to the top left edge of the image
-DrawLine(d_wand,0,0,radius,radius);
+DrawLine(d_wand,0,0,radius,radius)
 
-MagickDrawImage(m_wand,d_wand);
-MagickWriteImage(m_wand,"chart_test.jpg");
+MagickDrawImage(m_wand,d_wand)
+MagickWriteImage(m_wand,"chart_test1.jpg")
 
-c_wand = DestroyPixelWand(c_wand);
-m_wand = DestroyMagickWand(m_wand);
-d_wand = DestroyDrawingWand(d_wand);
 
-MagickWandTerminus();
+c_wand = DestroyPixelWand(c_wand)
+m_wand = DestroyMagickWand(m_wand)
+d_wand = DestroyDrawingWand(d_wand)
+
+MagickWandTerminus()
